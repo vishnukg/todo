ca1fd093231aa8da12218d2e56639cbe461435a0
Vishnu Ganesan <vishnukg@gmail.com>
2023-10-04 02:41:49 +1100

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
-import { Component } from '@angular/core';
-import { TodoItem } from './todoItem';
-import { TodoList } from './todoList';
+import { Component } from "@angular/core";
+import { TodoItem } from "./todoItem";
+import { TodoList } from "./todoList";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  private list = new TodoList('Bob', [
-    new TodoItem('Go for run', true),
-    new TodoItem('Get flowers'),
-    new TodoItem('Collect tickets'),
-  ]);
+    private list = new TodoList("Bob", [
+        new TodoItem("Go for run", true),
+        new TodoItem("Get flowers"),
+        new TodoItem("Collect tickets")
+    ]);
 
-  get username(): string {
-    return this.list.user;
-  }
+    get username(): string {
+        return this.list.user;
+    }
 
-  get itemCount(): number {
-    return this.list.items.filter((item) => !item.complete).length;
-  }
+    get itemCount(): number {
+        return this.list.items.filter((item) => !item.complete).length;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatTableModule } from "@angular/material/table";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatBadgeModule,
+        MatTableModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSlideToggleModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
